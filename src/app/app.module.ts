6e1e219e9a27126566746c806f1215f279387e63
sruthy-nr <sruthyn.@nestgroup.net>
2022-11-24 13:51:07 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,22 @@ import { AppComponent } from './app.component';
 import { PatientEntryComponent } from './patient-entry/patient-entry.component';
 import { PatientSearchComponent } from './patient-search/patient-search.component';
 import { PatientDeleteComponent } from './patient-delete/patient-delete.component';
+import { Router, Routes } from '@angular/router';
 
+const myRoute:Routes=[
+  {
+    path:"",
+    component:PatientEntryComponent
+  },
+  {
+    path:"search",
+    component:PatientSearchComponent
+  },
+  {
+    path:"delete",
+    component:PatientDeleteComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
